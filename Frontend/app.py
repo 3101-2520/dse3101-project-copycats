c8e00d9c3a96573098bb08a8d62d9791a340de36
yenfay <yenfaykung@gmail.com>
2026-03-13 22:54:48 +0800

    edits to app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,26 +1,28 @@
 import streamlit as st
 from components.new_backtest import new_backtest_button #for yenfay's code testing, DO NOT OVERWRITE PLEASE! 
-import pandas as pd
+from components.top_20 import top_20_table #for yenfay's code testing, DO NOT OVERWRITE PLEASE! 
 
+# page set up and layout
 st.set_page_config(
     page_title="dse3101 project",
     layout="wide"
 )
 
 st.title("Beginner Dashboard")
+c1, c2, c3, c4 = st.columns([0.7, 0.12, 0.12, 0.11])
 
-# new backtest
-new_backtest_button()
+with c1: 
+    st.write("")
+    
+with c2:
+    from_date = st.date_input("From:", key="from_date")
 
-# dummy data 
-d = {"Rank": [1, 2, 3], 
-     "Ticker": ["AAPL", "MSFT", "NVDA"],
-     "Allocation": [10.5, 2.1, 3.4],
-     "Recommended Allocation": ["12.5%", "11.0%", "5.0%"]}
+with c3:
+    to_date = st.date_input("To:", key="to_date")
+    
+with c4: 
+    new_backtest_button()
 
-dummy_data = pd.DataFrame(data=d, index=[1, 2, 3])
-
-# Create columns for side by side view of table and cards (col 1) and top 20 (col 2)
 col_left, col_right = st.columns([6, 4])
 
 with col_left:
@@ -31,4 +33,4 @@ with col_left:
 
 with col_right:
     st.header("Top 20 Stocks by Institutional Holdings")
-    st.table(data = dummy_data)
+    top_20_table()
