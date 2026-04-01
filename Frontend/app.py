36c4b0c7e2c80d13ecfef6d030697338c27e5d98
yenfay <yenfaykung@gmail.com>
2026-04-01 20:04:48 +0800

    edits to app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -14,11 +14,6 @@ st.set_page_config(
     page_title="dse3101 project",
     layout="wide"
 )
-<<<<<<< HEAD
-    
-# date layout
-c1, c2, c3, c4 = st.columns([0.6, 0.1, 0.1, 0.1])
-=======
 
 # title
 c_title, c_backtest = st.columns([8, 2], vertical_alignment="center")
@@ -26,7 +21,6 @@ with c_title:
     st.title("Dashboard")
 
 # date options
->>>>>>> db9cc3a (updated table and user input w dummy data)
 quarter_end_dates = [
     date(2024, 3, 31),
     date(2024, 6, 30),
@@ -38,9 +32,6 @@ quarter_end_dates = [
 c1, c2, c3, c4, c5, c6, c7 = st.columns([0.18, 0.16, 0.16, 0.16, 0.12, 0.12, 0.10])
 
 with c1:
-<<<<<<< HEAD
-    st.title("Copytrading Dashboard")
-=======
     initial_capital = st.number_input(
         "Initial Capital ($)",
         min_value=0,
@@ -48,7 +39,6 @@ with c1:
         step=1000,
         key="initial_capital"
     )
->>>>>>> db9cc3a (updated table and user input w dummy data)
 
 with c2:
     cost_rate = add_fees()
@@ -72,13 +62,6 @@ with c4:
         key="to_date"
     )
 
-<<<<<<< HEAD
-# configure left column for portfolio performance and right column for top 20 table
-col_left, col_right = st.columns([6, 4], gap = "large")
-
-with col_left:
-    st.header("Overview of Portfolio")
-=======
 with c5:
     topN = st.number_input(
         "Top N Stocks",
@@ -111,7 +94,6 @@ col_left, col_right = st.columns([6, 4])
 
 with col_left:
     st.header("Portfolio Performance")
->>>>>>> db9cc3a (updated table and user input w dummy data)
     portfolio_performance()
 
 with col_right:
